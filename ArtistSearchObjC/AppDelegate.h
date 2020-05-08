5af541b72b2a326ac7fab95182cfffdd5364e721
[jose-martinez] <alias_one11@outlook.com>
2020-05-07 20:09:05 -0400

    Initial Commit

---
+++ ArtistSearchObjC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ArtistSearchObjC
+//
+//  Created by Jose Martinez on 5/7/20.
+//  Copyright © 2020 Jose Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ ArtistSearchObjC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  ArtistSearchObjC
+//
+//  Created by Jose Martinez on 5/7/20.
+//  Copyright © 2020 Jose Martinez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
